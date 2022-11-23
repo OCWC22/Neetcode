dec0aab1862ad7b52c38cb19d77ecf979378a5a8
William Chen <57119977+OCWC22@users.noreply.github.com>
2022-11-22 18:27:37 -0800

    less code same tree py

--- Trees/SameTree.py
+++ Trees/SameTree.py
@@ -21,4 +21,14 @@ class Solution:
             return False
         if self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right):
             return True
+
+    #less code but same 
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        if not p and not q:
+            return True
+        if not q or not p or p.val != q.val:
+            return False
+        return self.isSameTree(p.right, q.right) and self.isSameTree(p.left, q.left)
+
+        
     
