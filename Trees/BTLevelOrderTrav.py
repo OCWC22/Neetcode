bb25dce27f324649a7cf4b8f864026616c434cdd
William Chen <57119977+OCWC22@users.noreply.github.com>
2022-11-27 23:13:34 -0800

    Binary Tree Level Order Traversal

---
+++ Trees/BTLevelOrderTrav.py
@@ -0,0 +1,31 @@
+#Leetcode 102
+#Binary Tree Level Order Traversal
+#https://leetcode.com/problems/binary-tree-level-order-traversal/solutions/
+#https://neetcode.io/practice
+
+import collections
+
+class TreeNode:
+    def __init__(self, val = 0, left = None, right = None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def levelOrder(self, root: TreeNode) -> List[List[int]]: 
+        res = []
+
+        q = collections.deque()
+        q.append(root)
+        while q:
+            level = []
+            for i in range(len(q)):
+                node = q.popleft()
+                if node:
+                    level.append(node.val)
+                    q.append(node.left)
+                    q.append(node.right)
+            if level:
+                res.append(level)
+
+        return res
